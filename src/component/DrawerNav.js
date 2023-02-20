0a8f1d31c51a4cef8d190051ee6650dfc18f34f2
as575909 <as575909@gmail.com>
2023-02-20 19:49:10 +0530

    fs

--- src/component/DrawerNav.js
+++ src/component/DrawerNav.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { Button, View } from 'react-native';
+import { View, Text } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 
 function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        onPress={() => navigation.navigate('Notifications')}
-        title="Go to notifications"
-      />
+     <Text> Welcome to Home Screen </Text>
     </View>
   );
 }
@@ -17,7 +14,7 @@ function HomeScreen({ navigation }) {
 function NotificationsScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
+     <Text> Welcome to Home Screen </Text>
     </View>
   );
 }
